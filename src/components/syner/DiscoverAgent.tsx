57e6f84e03728303acdb7b720aad09a0f09dfe77
xiaoyutuanzi <25206161@qq.com>
2023-11-08 14:07:25 +0800

    发现伙伴加图标

--- src/components/syner/DiscoverAgent.tsx
+++ src/components/syner/DiscoverAgent.tsx
@@ -7,15 +7,15 @@ const DiscoverAgent: React.FC = () => {
     return (
         <div className='agentItem'>
             <div className='homeIcon1'></div>
-            <img src="../../../res/css/img/syner/temp/agentpic.jpeg" alt="User Avatar" className="agentPicture" />
+            <img src="../../res/css/img/syner/temp/agentpic.jpeg" alt="User Avatar" className="agentPicture" />
             <div className="agentInfo">
                 <h3>余世维老师</h3>
                 <p>欢迎来到世维研习社，我是你们的老朋友世维</p>
             </div>
             <div className="agentStatus">
-                <span>100w+ 关注</span>
-                <span>75 评论</span>
-                <span>8 文章</span>
+                <span className='status1'>100w+ 关注</span>
+                <span className='status2'>75 评论</span>
+                <span className='status3'>8 文章</span>
             </div>
             <div className='homeIcon2'></div>
         </div>
