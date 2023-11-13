89c4fcde06ddeef70bccbba6974f7fe60ba85c48
Wang Yuan <asuka08@sina.com>
2023-11-13 13:46:13 +0800

    添加发现Agent，发现社区的组件

---
+++ src/components/syner/ ExploreSpace.tsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import '../../../res/css/syner/aiChat.css';
+
+const ExploreSpace: React.FC = () => {
+    
+
+    return (
+        <div>
+            发现社区
+        </div>
+
+    );
+}
+
+export default  ExploreSpace;
