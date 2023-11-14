07ce104c658236faf36ed4fa58cdff83e60b4c0f
Wang Yuan <asuka08@sina.com>
2023-11-14 11:33:05 +0800

    添加AIChat左边栏

---
+++ src/components/syner/LeftAgentChat.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const LeftAgentChat: React.FC = () => {
+
+    return (
+        <div>
+            这里是Agent左边栏
+        </div>
+    );
+}
+
+export default LeftAgentChat;
---
+++ src/components/syner/LeftLlmChat.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const LeftLlmChat: React.FC = () => {
+
+    return (
+        <div>
+            这里是Chat左边栏
+        </div>
+    );
+}
+
+export default LeftLlmChat;
